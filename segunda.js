43e2f712812af0e5df7166a9277be60e439ee77a
AgusFassola <agusfassolacarp@gmail.com>
2025-10-22 12:57:38 -0300

    filtro con javascript

--- segunda.js
+++ segunda.js
@@ -20,4 +20,47 @@ resultados?.addEventListener('click', (event)=>{
         boton.disabled=true;
         boton.classList.add('is-applied');
     }
+}); 
+
+const h2Busqueda= document.querySelector('.h2-busqueda');
+const jobs= document.querySelectorAll('.cajita');
+
+const filter = document.querySelector('#filter-location');
+filter?.addEventListener('change', (event)=>{
+    const location = event.target.value;
+    console.log(location);
+    if(location){
+        h2Busqueda.style.display = 'flex';
+        jobs.forEach((job)=>{
+            //const modalidad = job.dataset.modalidad;
+            const modalidad = job.getAttribute('data-modalidad');
+            if(location === "" || modalidad  === location){
+                job.style.display = 'flex';
+            }else{
+                job.style.display = 'none';
+            }
+        });
+    }  
+});
+
+
+
+const buscador= document.querySelector('#buscador');
+buscador?.addEventListener('input', (event)=>{
+    const searchTerm= event.target.value.toLowerCase();
+    console.log('Buscando empleos que coincidan con:', searchTerm);
+});
+
+buscador.addEventListener('blur', (event)=>{
+    console.log('El buscador perdió el foco');
+});
+
+const form= document.querySelector('#empleos-form');
+form?.addEventListener('submit', (event)=>{
+    event.preventDefault(); //evita que se recargue la página
+    const formData= new FormData(form);
+});
+
+const tecla= document.addEventListener('keydown', (event)=>{
+    console.log('Tecla presionada:', event.key);
 });

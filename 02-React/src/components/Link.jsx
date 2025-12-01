6cc53016faf777dba37197e901f5de833828d24c
AgusFassola <agusfassolacarp@gmail.com>
2025-11-30 23:57:43 -0300

    jobdetails

--- 02-React/src/components/Link.jsx
+++ 02-React/src/components/Link.jsx
@@ -1,7 +1,6 @@
 import { Link as NavLink } from 'react-router'
 export default function Link({ href, children, ...restOfProps }) {
 
-
     return (
     <NavLink to={href} {...restOfProps} >
       {children}
--- 02-React/src/pages/Detail.jsx
+++ 02-React/src/pages/Detail.jsx
@@ -1,11 +1,59 @@
-import { useParams } from "react-router";
+import { useParams, useNavigate, Link } from "react-router";
+import { useState, useEffect } from 'react';
+
 export function JobDetail() {
     const { id } = useParams();
+    const Navigate = useNavigate();
+
+    const [job, setJob] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+      fetch(`https://jscamp-api.vercel.app/api/jobs?${id}`)
+        .then((response) =>{
+          if (!response.ok) throw new Error('job not found');
+          response.json()
+        }) 
+        .then((data) => {
+          setJob(data);
+        })
+        .catch((error) => {
+          setError(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }, [id])
+
+    if(loading){
+      return <div style={{maxWidth:'1280px', margin: '0 auto', padding:'0 1rem'}}>
+        <div>
+          <p>Cargando...</p>
+        </div>
+      </div>;
+    }
+
+    if(error || !job){
+      return(
+       <div style={{maxWidth:'1280px', margin: '0 auto', padding:'0 1rem'}}>
+        <div>
+          <h2>Error al cargar el empleo</h2>
+          <button onClick={()=> Navigate('/')}>
+            Volver al inicio
+          </button>
+        </div>
+      </div>
+      );
+    }
+
   return (
-    <div>
-      <h2>Job Detail Page</h2>
-      <p>Details about a specific job will be shown here.</p>
-        {id}
+    <div style={{maxWidth:'1280px', margin: '0 auto', padding:'0 1rem'}}>
+      <nav>
+        <Link
+          hrefLang="/search"
+        >Empleos</Link>
+      </nav>
     </div>
   );
 }
